bc1cc98f4dd4fea3a3dfdbd620def083160a3233
Václav Chlumský <chlumskyvaclav@gmail.com>
2021-09-26 15:18:41 +0200

    remove screenshot from release

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip -r wireless-hid@chlumskyvaclav.gmail.com.zip . --exclude=po/\* --exclude=.git/\* --exclude=*.sh --exclude=*.zip
+zip -r wireless-hid@chlumskyvaclav.gmail.com.zip . --exclude=po/\* --exclude=.git/\* --exclude=screenshot.png --exclude=*.sh --exclude=*.zip
 
