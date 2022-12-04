b30301eff790b2c56fecee4ea4dd1b7d2382e190
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-12-04 20:50:53 +0000

    Update build script to include settings and GUI files

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-zip -r wireless-hid@chlumskyvaclav.gmail.com.zip . --exclude=po/\* --exclude=.git/\* --exclude=screenshot.png --exclude=*.sh --exclude=*.zip
+#Requires libglib2.0-bin
+glib-compile-schemas schemas
 
+#Build and rename the zip
+gnome-extensions pack --force --extra-source=LICENSE --extra-source=README.md --extra-source=CHANGELOG.md --extra-source=ui --extra-source=wirelesshid.js
+mv "wireless-hid@chlumskyvaclav.gmail.com.shell-extension.zip" "wireless-hid@chlumskyvaclav.gmail.com.zip"
