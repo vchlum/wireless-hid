7e227248263f8ce32786ee3249dd41fe643171c0
Gianni Lerro <glerro@pm.me>
2023-01-31 23:02:35 +0100

    Revamp Preferences UI.

--- prefs.js
+++ prefs.js
@@ -1,112 +1,133 @@
+'use strict';
+
+/**
+ * extension wireless-hid
+ * JavaScript Gnome extension for wireless keyboards and mice.
+ *
+ * @author Václav Chlumský
+ * @copyright Copyright 2021, Václav Chlumský.
+ */
+
+ /**
+ * @license
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Václav Chlumský
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const ShellVersion = parseFloat(imports.misc.config.PACKAGE_VERSION);
 
-const { Gtk, Gio } = imports.gi;
+const { Gdk, Gtk, Gio } = imports.gi;
 const Adw = ShellVersion >= 42 ? imports.gi.Adw : null;
 
-var PrefsPages = class PrefsPages {
-  constructor() {
-    this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.wireless-hid');
+var PrefsPage = class PrefsPage {
+    constructor() {
+        this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.wireless-hid');
 
-    this._builder = new Gtk.Builder();
-    this._builder.set_translation_domain(Me.metadata.uuid);
+        // Load css style for Gnome < 42
+        const cssProvider = new Gtk.CssProvider();
+        cssProvider.load_from_path(Me.path + '/prefs.css');
 
-    this.createPreferences();
-  }
+        if (ShellVersion >= 40 && ShellVersion < 42 ) {
+            Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(),
+                cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
+        } else if (ShellVersion < 40) {
+            Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
+                cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
+        }
 
-  createPreferences() {
-    //Use different UI file for GNOME 40+ and 3.38
-    if (ShellVersion >= 40) {
-      this._builder.add_from_file(Me.path + '/ui/prefs-gtk4.ui');
-    } else {
-      this._builder.add_from_file(Me.path + '/ui/prefs.ui');
+        // Create a new Builder to load UI
+        this._builder = new Gtk.Builder();
+        // this._builder.set_translation_domain(Me.metadata.uuid);
+
+        this.createPreferences();
     }
 
-    //Get the settings container widget
-    this.preferencesWidget = this._builder.get_object('main-prefs');
+    createPreferences() {
+        // Use different UI file for GNOME 42+, 40+ and 3.36+
+        if (ShellVersion >= 42) {
+            this._builder.add_from_file(Me.path + '/ui/prefs-adw1.ui');
+        } else if (ShellVersion >= 40) {
+            this._builder.add_from_file(Me.path + '/ui/prefs-gtk4.ui');
+        } else {
+            this._builder.add_from_file(Me.path + '/ui/prefs-gtk3.ui');
+        }
 
-    this.settingElements = {
-      'hide-unknown-states-switch': {
-        'settingKey': 'hide-unknown-battery-state',
-        'bindProperty': 'active'
-      },
-      'hide-elan-switch': {
-        'settingKey': 'hide-elan',
-        'bindProperty': 'active'
-      }
+        // Get the settings container widget
+        this.preferencesWidget = this._builder.get_object('main-prefs');
+
+        this.settingElements = {
+            'hide-unknown-states-switch': {
+                'settingKey': 'hide-unknown-battery-state',
+                'bindProperty': 'active'
+            },
+            'hide-elan-switch': {
+                'settingKey': 'hide-elan',
+                'bindProperty': 'active'
+            }
+        }
+
+        // Loop through settings toggles and dropdowns and bind together
+        Object.keys(this.settingElements).forEach((element) => {
+            this._settings.bind(
+                this.settingElements[element].settingKey,   // GSettings key to bind to
+                this._builder.get_object(element),          // GTK UI element to bind to
+                this.settingElements[element].bindProperty, // The property to share
+                Gio.SettingsBindFlags.DEFAULT
+            );
+        });
     }
-
-    //Loop through settings toggles and dropdowns and bind together
-    Object.keys(this.settingElements).forEach((element) => {
-      this._settings.bind(
-        this.settingElements[element].settingKey, //GSettings key to bind to
-        this._builder.get_object(element), //GTK UI element to bind to
-        this.settingElements[element].bindProperty, //The property to share
-        Gio.SettingsBindFlags.DEFAULT
-      );
-    });
-  }
 }
 
 function init() {
-  ExtensionUtils.initTranslations();
+  // ExtensionUtils.initTranslations();
 }
 
 //Create preferences window for GNOME 42+
 function fillPreferencesWindow(window) {
-  //Create page and widget
-  let prefsPages = new PrefsPages();
-  let settingsPage = new Adw.PreferencesPage();
-  let settingsGroup = new Adw.PreferencesGroup();
-
-  //Build the settings page and add to window
-  settingsPage.set_title('Settings');
-  settingsPage.set_icon_name('preferences-system-symbolic');
-  settingsGroup.add(prefsPages.preferencesWidget);
-  settingsPage.add(settingsGroup);
-  window.add(settingsPage);
+    let prefsPage = new PrefsPage();
+    window.set_default_size(600, 290);
+    window.add(prefsPage.preferencesWidget);
 }
 
-//Create preferences window for GNOME 3.38-41
+//Create preferences window for GNOME 3.38 to 41
 function buildPrefsWidget() {
-  let prefsPages = new PrefsPages();
-  let settingsWindow = new Gtk.ScrolledWindow();
+    let prefsPage = new PrefsPage();
 
-  //Use a stack to store pages
-  let pageStack = new Gtk.Stack();
-  pageStack.add_titled(prefsPages.preferencesWidget, 'settings', 'Settings');
-
-  let pageSwitcher = new Gtk.StackSwitcher();
-  pageSwitcher.set_stack(pageStack);
-
-  //Add the stack to the scrolled window
-  if (ShellVersion >= 40) {
-    settingsWindow.set_child(pageStack);
-  } else {
-    settingsWindow.add(pageStack);
-  }
-
-  //Enable all elements differently for GNOME 40+ and 3.38
-  if (ShellVersion >= 40) {
-    settingsWindow.show();
-  } else {
-    settingsWindow.show_all();
-  }
-
-  //Modify top bar to add a page menu, when the window is ready
-  settingsWindow.connect('realize', () => {
-    let window = ShellVersion >= 40 ? settingsWindow.get_root() : settingsWindow.get_toplevel();
-    let headerBar = window.get_titlebar();
-
-    //Add page switching menu to header
-    if (ShellVersion >= 40) {
-      headerBar.set_title_widget(pageSwitcher);
-    } else {
-      headerBar.set_custom_title(pageSwitcher);
+    if (prefsPage.preferencesWidget.show_all) {
+        prefsPage.preferencesWidget.show_all();
     }
-    pageSwitcher.show();
-  });
 
-  return settingsWindow;
+    prefsPage.preferencesWidget.connect('realize', () => {
+        let window = ShellVersion >= 40 ? prefsPage.preferencesWidget.get_root() :
+            prefsPage.preferencesWidget.get_toplevel();
+        if (ShellVersion >= 40) {
+            window.set_default_size(600, 205);
+        } else {
+            window.resize(600, 205);
+        }
+    });
+
+    return prefsPage.preferencesWidget;
 }
+
