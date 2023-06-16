de24524564875bd823d45710b686417e0f4a3b39
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2023-06-16 18:41:03 +0100

    Update release.sh for compatibility

--- release.sh
+++ release.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Requires libglib2.0-bin
-glib-compile-schemas schemas
-
-#Build and rename the zip
-gnome-extensions pack --force --extra-source=LICENSE --extra-source=README.md --extra-source=CHANGELOG.md --extra-source=ui --extra-source=wirelesshid.js --extra-source=prefs.css
-mv "wireless-hid@chlumskyvaclav.gmail.com.shell-extension.zip" "wireless-hid@chlumskyvaclav.gmail.com.zip"
+make clean
+make build
+mv build/wireless-hid@chlumskyvaclav.gmail.com.zip ./wireless-hid@chlumskyvaclav.gmail.com.zip
