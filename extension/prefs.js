a1675162f0a4d01bd159ceb3f8db0ab81ddf2fac
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2023-08-18 23:12:49 +0100

    Drop GNOME compatibility code from preferences

--- extension/prefs.js
+++ extension/prefs.js
@@ -35,43 +35,23 @@
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const ShellVersion = parseFloat(imports.misc.config.PACKAGE_VERSION);
 
 const { Gdk, Gtk, Gio } = imports.gi;
-const Adw = ShellVersion >= 42 ? imports.gi.Adw : null;
+const Adw = imports.gi.Adw;
 
 var PrefsPage = class PrefsPage {
     constructor() {
         this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.wireless-hid');
 
-        // Load css style for Gnome < 42
-        const cssProvider = new Gtk.CssProvider();
-        cssProvider.load_from_path(Me.path + '/prefs.css');
-
-        if (ShellVersion >= 40 && ShellVersion < 42 ) {
-            Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(),
-                cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
-        } else if (ShellVersion < 40) {
-            Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
-                cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
-        }
-
         // Create a new Builder to load UI
         this._builder = new Gtk.Builder();
-        // this._builder.set_translation_domain(Me.metadata.uuid);
 
         this.createPreferences();
     }
 
     createPreferences() {
-        // Use different UI file for GNOME 42+, 40+ and 3.36+
-        if (ShellVersion >= 42) {
-            this._builder.add_from_file(Me.path + '/ui/prefs-adw1.ui');
-        } else if (ShellVersion >= 40) {
-            this._builder.add_from_file(Me.path + '/ui/prefs-gtk4.ui');
-        } else {
-            this._builder.add_from_file(Me.path + '/ui/prefs-gtk3.ui');
-        }
+        // Load libadwaita UI file
+        this._builder.add_from_file(Me.path + '/ui/prefs-adw1.ui');
 
         // Get the settings container widget
         this.preferencesWidget = this._builder.get_object('main-prefs');
@@ -111,35 +91,10 @@ var PrefsPage = class PrefsPage {
     }
 }
 
-function init() {
-  // ExtensionUtils.initTranslations();
-}
-
-//Create preferences window for GNOME 42+
+//Create preferences window
 function fillPreferencesWindow(window) {
     let prefsPage = new PrefsPage();
     window.set_default_size(600, 435);
     window.add(prefsPage.preferencesWidget);
-}
-
-//Create preferences window for GNOME 3.38 to 41
-function buildPrefsWidget() {
-    let prefsPage = new PrefsPage();
-
-    if (prefsPage.preferencesWidget.show_all) {
-        prefsPage.preferencesWidget.show_all();
-    }
-
-    prefsPage.preferencesWidget.connect('realize', () => {
-        let window = ShellVersion >= 40 ? prefsPage.preferencesWidget.get_root() :
-            prefsPage.preferencesWidget.get_toplevel();
-        if (ShellVersion >= 40) {
-            window.set_default_size(600, 435);
-        } else {
-            window.resize(600, 435);
-        }
-    });
-
-    return prefsPage.preferencesWidget;
 }
 
